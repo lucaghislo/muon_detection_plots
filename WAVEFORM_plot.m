6c2c810291591674f0520337021f37e736d29c66
Luca Ghislotti <ghislottiluca@gmail.com>
2022-08-25 18:30:00 +0200

    Added and updated plots

--- WAVEFORM_plot.m
+++ WAVEFORM_plot.m
@@ -49,7 +49,7 @@ for pt = 0:7
     legend(channels, 'NumColumns', 2, 'Location','eastoutside')
     xlabel('Time [$\mu$s]');
     xticklabels([0:0.5:3])
-    ylabel('Channel\_out [ADC code]');
+    ylabel('Channel Output [ADU]');
     ylim([50 850])
     set(gcf, 'Color', 'w');
     set(gca,'fontname','Computer Modern') 
@@ -62,6 +62,58 @@ for pt = 0:7
     ax.Legend.FontSize = fontsize; 
     f.Position = [200 160 900  550];
     exportgraphics(gcf,['output/WAVEFORM_ASIC_Tamb_pt', num2str(pt) ,'.pdf'],'ContentType','vector');
+end
+
+
+%% ASIC a a -40C
+
+data = readtable('input/WaveformScan_ASIC_-40C.dat');
+time = data.tc(1:120);
+ch_out = nan(size(time, 1), 32);
+counter = 1;
+
+for pt = 0:7
+    for ch = 0:31
+        counter = 1;
+        for i = 1:size(data, 1)
+            if data.pt(i) == pt
+                if data.ch(i) == ch
+                    ch_out(counter, ch+1) = table2array(data(i, "mean"));
+                    counter = counter + 1;
+                end
+            end
+        end
+    end
+    
+    f = figure('Visible', 'on');
+    colors = distinguishable_colors(32, 'w');
+    hold on
+    for j = 1:size(ch_out, 2)
+        plot(time, ch_out(:, j), 'Color', [colors(j, 1), colors(j, 2), colors(j, 3)])
+    end
+    hold off
+    
+    channels = strings(32, 1);
+    for ch = 0:31
+        channels(ch+1, 1) = strcat("Ch \#", num2str(ch));
+    end
+    
+    legend(channels, 'NumColumns', 2, 'Location','eastoutside')
+    xlabel('Time [$\mu$s]');
+    xticklabels([0:0.5:3])
+    ylabel('Channel Output [ADU]');
+    ylim([50 850])
+    set(gcf, 'Color', 'w');
+    set(gca,'fontname','Computer Modern') 
+    grid on
+    box on
+    
+    ax = gca; 
+    ax.XAxis.FontSize = fontsize; 
+    ax.YAxis.FontSize = fontsize; 
+    ax.Legend.FontSize = fontsize; 
+    f.Position = [200 160 900  550];
+    exportgraphics(gcf,['output/WAVEFORM_ASIC_-40C_pt', num2str(pt) ,'.pdf'],'ContentType','vector');
 end
 
 
@@ -105,7 +157,7 @@ end
 %legend(peaking_times, 'Location', 'eastoutside')
 xlabel('Time [$\mu$s]');
 xticklabels([0:0.5:3])
-ylabel('Channel\_out [ADC code]');
+ylabel('Channel Output [ADU]');
 ylim([50 750])
 set(gcf, 'Color', 'w');
 set(gca,'fontname','Computer Modern') 
@@ -119,6 +171,61 @@ ax.YAxis.FontSize = fontsize;
 %f.Position = [200 160 900  550];
 exportgraphics(gcf,'output/WAVEFORM_ASIC_Tamb_allpt.pdf','ContentType','vector');
 
+
+%% ASIC a -40C: mean WAVEFORM
+
+clear; clc;
+fontsize = 12;
+
+data = readtable('input/WaveformScan_ASIC_-40C.dat');
+time = data.tc(1:120);
+ch_out = nan(size(time, 1), 32);
+counter = 1;
+
+f = figure('Visible', 'on');
+colors = distinguishable_colors(8, 'w');
+
+hold on
+for pt = 0:7
+    for ch = 0:31
+        counter = 1;
+        for i = 1:size(data, 1)
+            if data.pt(i) == pt
+                if data.ch(i) == ch
+                    ch_out(counter, ch+1) = table2array(data(i, "mean"));
+                    counter = counter + 1;
+                end
+            end
+        end
+    end
+     
+    plot(time, mean(ch_out, 2), 'Color', [colors(pt+1, 1), colors(pt+1, 2), colors(pt+1, 3)])
+end
+hold off
+
+peaking_times = strings(8, 1);
+peaking_times_values = [0.30 0.47 0.65 0.82 1.00 1.27 1.54 1.82] % microsecondi
+for pt = 0:7
+    peaking_times(pt+1, 1) = strcat("$\tau_{", num2str(pt), '}$ = ', " ", num2str(peaking_times_values(pt+1), '%.2f'), ' $\mu$s');
+end
+
+%legend(peaking_times, 'Location', 'eastoutside')
+xlabel('Time [$\mu$s]');
+xticklabels([0:0.5:3])
+ylabel('Channel Output [ADU]');
+ylim([50 700])
+set(gcf, 'Color', 'w');
+set(gca,'fontname','Computer Modern') 
+grid on
+box on
+
+ax = gca; 
+ax.XAxis.FontSize = fontsize; 
+ax.YAxis.FontSize = fontsize; 
+%ax.Legend.FontSize = fontsize; 
+%f.Position = [200 160 900  550];
+exportgraphics(gcf,'output/WAVEFORM_ASIC_-40C_allpt.pdf','ContentType','vector');
+print(f, 'output/WAVEFORM_ASIC_-40C_allpt','-dsvg')
 
 
 %% FEB a temperatura ambiente
@@ -157,7 +264,7 @@ for pt = 0:7
     legend(channels, 'NumColumns', 2, 'Location','eastoutside')
     xlabel('Time [$\mu$s]');
     xticklabels([0:0.5:3])
-    ylabel('Channel\_out [ADC code]');
+    ylabel('Channel Output [ADU]');
     ylim([50 850])
     set(gcf, 'Color', 'w');
     set(gca,'fontname','Computer Modern') 
@@ -213,7 +320,7 @@ end
 %legend(peaking_times, 'Location', 'eastoutside')
 xlabel('Time [$\mu$s]');
 xticklabels([0:0.5:3])
-ylabel('Channel\_out [ADC code]');
+ylabel('Channel Output [ADU]');
 ylim([50 750])
 set(gcf, 'Color', 'w');
 set(gca,'fontname','Computer Modern') 
@@ -264,7 +371,7 @@ for pt = 0:7
     legend(channels, 'NumColumns', 2, 'Location','eastoutside')
     xlabel('Time [$\mu$s]');
     xticklabels([0:0.5:3])
-    ylabel('Channel\_out [ADC code]');
+    ylabel('Channel Output [ADU]');
     ylim([50 850])
     set(gcf, 'Color', 'w');
     set(gca,'fontname','Computer Modern') 
@@ -320,7 +427,7 @@ end
 legend(peaking_times, 'Location', 'eastoutside')
 xlabel('Time [$\mu$s]');
 xticklabels([0:0.5:3])
-ylabel('Channel\_out [ADC code]');
+ylabel('Channel Output [ADU]');
 ylim([50 750])
 set(gcf, 'Color', 'w');
 set(gca,'fontname','Computer Modern') 
@@ -371,7 +478,7 @@ for pt = 0:7
     legend(channels, 'NumColumns', 2, 'Location','eastoutside')
     xlabel('Time [$\mu$s]');
     xticklabels([0:0.5:3])
-    ylabel('Channel\_out [ADC code]');
+    ylabel('Channel Output [ADU]');
     ylim([50 850])
     set(gcf, 'Color', 'w');
     set(gca,'fontname','Computer Modern') 
@@ -427,7 +534,7 @@ end
 legend(peaking_times, 'Location', 'eastoutside')
 xlabel('Time [$\mu$s]');
 xticklabels([0:0.5:3])
-ylabel('Channel\_out [ADC code]');
+ylabel('Channel Output [ADU]');
 ylim([50 750])
 set(gcf, 'Color', 'w');
 set(gca,'fontname','Computer Modern') 
